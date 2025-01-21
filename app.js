e67fb9434017fc2b245475e83a2a2365c2deb4b5
Fahim047 <fahimulislam58@gmail.com>
2025-01-21 20:52:46 +0600

    🎉 Project Initialized

---
+++ app.js
@@ -0,0 +1,20 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res
+		.status(err.status || 500)
+		.json({ message: err.message || 'Internal Server Error!' });
+});
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+export default app;
