2f4953fab12452cc8df5d3ee5581b9a2036a8fa0
Fahim047 <fahimulislam58@gmail.com>
2025-01-27 17:20:38 +0600

    ✅ Fetched classes in the all classes page of admin dashboard.

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,3 +1,4 @@
+import Class from '../models/class.model.js';
 import TeacherRequest from '../models/teacherRequest.model.js';
 import User from '../models/user.model.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
@@ -28,4 +29,18 @@ export const acceptTeacherRequest = asyncHandler(async (req, res) => {
 		{ role: 'teacher' }
 	);
 	return res.status(200).json({ message: 'Teacher request accepted!' });
+});
+export const getAllClasses = asyncHandler(async (req, res) => {
+	const classes = await Class.find().lean();
+	return res.status(200).json(classes);
+});
+export const approveClass = asyncHandler(async (req, res) => {
+	const { id } = req.params;
+	const classToApprove = await Class.findById(id);
+	if (!classToApprove) {
+		return res.status(404).json({ error: 'Class not found' });
+	}
+	classToApprove.status = 'approved';
+	await classToApprove.save();
+	return res.status(200).json({ message: 'Class approved' });
 });
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import {
 	acceptTeacherRequest,
+	approveClass,
+	getAllClasses,
 	getTeacherRequests,
 	rejectTeacherRequest,
 } from '../controllers/admin.controller.js';
@@ -10,5 +12,7 @@ const router = Router();
 router.route('/teacher-requests').get(getTeacherRequests);
 router.route('/teacher-requests/:id/reject').patch(rejectTeacherRequest);
 router.route('/teacher-requests/:id/accept').patch(acceptTeacherRequest);
+router.route('/classes').get(getAllClasses);
+router.route('/classes/:id/approve').patch(approveClass);
 
 export default router;
